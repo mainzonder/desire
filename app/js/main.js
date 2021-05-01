a079a84be6ca6868e3921f166efbf847aef371eb
mainzonder <no7au53r@gmail.com>
2021-05-01 20:08:32 +0300

    add mobile version

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,14 @@ $(function () {
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
     })
+
+
+
+$('.header__btn-menu').on('click', function (){
+    $('.menu').toggleClass('menu--open')
+})
+
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
