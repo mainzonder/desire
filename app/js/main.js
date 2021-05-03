3953a5ac6aa2e5110724831c3259554f661c64e6
mainzonder <no7au53r@gmail.com>
2021-05-03 15:14:21 +0300

    adaptive finish

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,13 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close')
     })
 
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open')
+    })
 
-
-$('.header__btn-menu').on('click', function (){
-    $('.menu').toggleClass('menu--open')
-})
-
+    if ($(window).width() < 651) {
+        $('.works-path__item--measuring').appendTo($('.works-path__items-box'))
+    }
 
     $('.top__slider').slick({
         dots: true,
@@ -25,7 +26,43 @@ $('.header__btn-menu').on('click', function (){
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                },
+
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                },
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                },
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+                // breakpoint: 375,
+                // settings: {
+                //     slidesToShow: 1,
+                //     slidesToScroll: 2,
+                // },
+            },
+        ],
     })
+
     $('.article-slider__box').slick({
         prevArrow:
             '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src=images/arrow-slide-left.svg alt="arrow"></button>',
